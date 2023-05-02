db5bfa525f1c9e4d6d6386e9a43bf899c507acaf
muhammed-sayeed <sayeedmon25@gmail>
2023-05-02 17:00:39 +0530

    500

--- controller/admincontroller/tagcontroller.js
+++ controller/admincontroller/tagcontroller.js
@@ -19,7 +19,7 @@ const addTag = async(req,res)=>{
         success:true
     })
      }catch(e){
-      res.status('500').json('int server error')
+      res.status(500).json('int server error')
      }
     
   }
