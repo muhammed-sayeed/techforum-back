a25510d0996ac9babf0daa0744c8337035e025c0
muhammed-sayeed <sayeedmon25@gmail>
2023-06-27 10:10:09 +0530

    updation

--- controller/usercontrller/questioncontroller.js
+++ controller/usercontrller/questioncontroller.js
@@ -2,6 +2,8 @@ const QN = require('../../models/questions')
 const Tags = require('../../models/tag')
 const user = require('../../models/user')
 const Badge = require('../../models/badge')
+const Filter  =require('bad-words')
+const filter = new Filter()
 
 const { findOneAndUpdate } = require('../../models/user');
 const socket = require('socket.io')
@@ -11,13 +13,37 @@ const addQn = async (req, res) => {
  try{
   const cHtml = req.body.CHtml;
   const Content = req.body.Content;
-  const editor = req.body.editorContent;
+  let editor = req.body.editorContent;
   const tags = req.body.tags
+
+  let searchTextRegex =/(<pre\b[^>]>)([\s\S]?)(<\/pre>)/gi;
+  editor =  editor.replace(searchTextRegex,`<div class=" bg-gray-800 rounded-lg overflow-hidden mt-6">
+  <div class="flex justify-between">
+      <div id="header-buttons" class="py-3 px-4 flex">
+          <div class="rounded-full w-3 h-3 bg-red-500 mr-2"></div>
+          <div class="rounded-full w-3 h-3 bg-yellow-500 mr-2"></div>
+          <div class="rounded-full w-3 h-3 bg-green-500"></div>
+      </div>
+      <button  class="p-2 text-center bg-gray-800 text-gray-100 hover:bg-gray-700 rounded flex justify-between items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none" class="pr-1">
+              <path fill="currentColor" fill-rule="evenodd" d="M4 2a2 2 0 00-2 2v9a2 2 0 002 2h2v2a2 2 0 002 2h9a2 2 0 002-2V8a2 2 0 00-2-2h-2V4a2 2 0 00-2-2H4zm9 4V4H4v9h2V8a2 2 0 012-2h5zM8 8h9v9H8V8z"/>
+          </svg>
+          Copy
+      </button>
+  </div>
+  <div class="w-full mx-2 my-2 overflow-auto">
+  $1$2$3
+  </div>
+</div>`);
 
   const data = res.locals.jwt_user;
   const email = data.email;
   const Id = await user.findOne({email:email},{_id:1})
  const id = Id._id.toString()
+
+ if(filter.isProfane(Content)){
+  return res.status(200).json("Don't use false langaugue")
+ }
 
   const qns = new QN({
     title: Content,
